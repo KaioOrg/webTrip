5db88e43a684474e393de5758538802ccda50a87
KaioOrg <maksufa3@gmail.com>
2024-03-31 16:10:21 +0500

    hg

---
+++ backendApp.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template("mainPage.html")
+
+
+@app.route('/about')
+def about_me_page():
+    return render_template("PageAboutMe.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
