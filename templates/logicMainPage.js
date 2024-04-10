7e9ea9a966600ac153464b8a7d7f3a9daf17e2e8
KaioOrg <maksufa3@gmail.com>
2024-04-10 23:52:35 +0500

    update 1.0

--- templates/logicMainPage.js
+++ templates/logicMainPage.js
@@ -1,7 +1,26 @@
+let isPushedButton = false
+const activityIcon = document.getElementsByClassName("activity-text-button")
+const entertainmentIcon = document.getElementsByClassName("entertainment-text-button")
+const mallsIcon = document.getElementsByClassName("malls-text-button")
+
 function changeOnMainPage(){
     window.location.href = "index.html"
 }
 
 function changeOnSearchPage(){
     window.location.href = "searchPage.html"
+}
+
+let newIconHotel = document.createElement('newIcon');
+function changeColorOnHotelButtonFilter(){
+    if(isPushedButton === false){
+        document.querySelector(".categories-item").style.backgroundColor = "#6495ed"
+        document.querySelector(".text-category").style.color = "#ffffff"
+        $(".hotel-text-button").addClass("hotel-new-icon")
+        isPushedButton = true
+    }else if(isPushedButton === true){
+        document.querySelector(".categories-item").style.backgroundColor = "#ffffff"
+        document.querySelector(".text-category").style.color = "#72727E"
+        isPushedButton = false
+    }
 }
