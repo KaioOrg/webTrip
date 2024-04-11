693eadf887569288e42b81b0a53e61044517f57c
KaioOrg <maksufa3@gmail.com>
2024-04-12 02:28:23 +0500

    update 1.0

--- templates/logicMainPage.js
+++ templates/logicMainPage.js
@@ -1,7 +1,10 @@
 let isPushedHotelButton = false
 let isPushedEntertainmentButton = false
-let isPushedActivityButton = false
+let isPushedParkButton = false
 let isPushedMallButton = false
+let isPushedRestaurantButton = false
+let isPushedTheatreButton = false
+let isPushedMuseumButton = false
 
 function changeOnMainPage(){
     window.location.href = "index.html"
@@ -17,7 +20,6 @@ function changeColorOnHotelButtonFilter(){
         document.querySelector(".hotel-text-button").style.color = "#ffffff"
         $(".hotel-text-button").addClass("hotel-new-icon")
         // document.querySelector(".all-places-searched").style.display = "none"
-        isPushedHotelButton = true
     }else if(isPushedHotelButton === true){
         document.querySelector(".hotel-button-filter").style.backgroundColor = "#ffffff"
         document.querySelector(".hotel-text-button").style.color = "#72727E"
@@ -40,17 +42,17 @@ function changeColorOnEntertainmentButtonFilter(){
     }
 }
 
-function changeColorOnActivityButtonFilter(){
-    if(isPushedActivityButton === false){
-        document.querySelector(".activity-button-filter").style.backgroundColor = "#6495ed"
-        document.querySelector(".activity-text-button").style.color = "#ffffff"
-        $(".activity-text-button").addClass("activity-new-icon")
-        isPushedActivityButton = true
-    }else if(isPushedActivityButton === true) {
-        document.querySelector(".activity-button-filter").style.backgroundColor = "#ffffff"
-        document.querySelector(".activity-text-button").style.color = "#72727E"
-        $(".activity-new-icon").addClass("activity-text-button").removeClass("activity-new-icon")
-        isPushedActivityButton = false
+function changeColorOnParkButtonFilter(){
+    if(isPushedParkButton === false){
+        document.querySelector(".park-button-filter").style.backgroundColor = "#6495ed"
+        document.querySelector(".park-text-button").style.color = "#ffffff"
+        $(".park-text-button").addClass("park-new-icon")
+        isPushedParkButton = true
+    }else if(isPushedParkButton === true) {
+        document.querySelector(".park-button-filter").style.backgroundColor = "#ffffff"
+        document.querySelector(".park-text-button").style.color = "#72727E"
+        $(".park-new-icon").addClass("park-text-button").removeClass("park-new-icon")
+        isPushedParkButton = false
     }
 }
 
@@ -65,5 +67,47 @@ function changeColorOnMallButtonFilter(){
         document.querySelector(".malls-text-button").style.color = "#72727E"
         $(".malls-new-icon").addClass("malls-text-button").removeClass("malls-new-icon")
         isPushedMallButton = false
+    }
+}
+
+function changeColorOnRestaurantButtonFilter(){
+    if(isPushedRestaurantButton === false){
+        document.querySelector(".restaurant-button-filter").style.backgroundColor = "#6495ed"
+        document.querySelector(".restaurant-text-button").style.color = "#ffffff"
+        $(".restaurant-text-button").addClass("restaurant-new-icon")
+        isPushedRestaurantButton = true
+    }else if(isPushedRestaurantButton === true) {
+        document.querySelector(".restaurant-button-filter").style.backgroundColor = "#ffffff"
+        document.querySelector(".restaurant-text-button").style.color = "#72727E"
+        $(".restaurant-new-icon").addClass("restaurant-text-button").removeClass("restaurant-new-icon")
+        isPushedRestaurantButton = false
+    }
+}
+
+function changeColorOnTheatreButtonFilter(){
+    if(isPushedTheatreButton === false){
+        document.querySelector(".theatre-button-filter").style.backgroundColor = "#6495ed"
+        document.querySelector(".theatre-text-button").style.color = "#ffffff"
+        $(".theatre-text-button").addClass("theatre-new-icon")
+        isPushedTheatreButton = true
+    }else if(isPushedTheatreButton === true) {
+        document.querySelector(".theatre-button-filter").style.backgroundColor = "#ffffff"
+        document.querySelector(".theatre-text-button").style.color = "#72727E"
+        $(".theatre-new-icon").addClass("theatre-text-button").removeClass("theatre-new-icon")
+        isPushedTheatreButton = false
+    }
+}
+
+function changeColorOnMuseumButtonFilter(){
+    if(isPushedMuseumButton === false){
+        document.querySelector(".museum-button-filter").style.backgroundColor = "#6495ed"
+        document.querySelector(".museum-text-button").style.color = "#ffffff"
+        $(".museum-text-button").addClass("museum-new-icon")
+        isPushedMuseumButton = true
+    }else if(isPushedMuseumButton === true) {
+        document.querySelector(".museum-button-filter").style.backgroundColor = "#ffffff"
+        document.querySelector(".museum-text-button").style.color = "#72727E"
+        $(".museum-new-icon").addClass("museum-text-button").removeClass("museum-new-icon")
+        isPushedMuseumButton = false
     }
 }
