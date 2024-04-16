22ab31553f802fe17d125376408a4d679de3cf57
KaioOrg <maksufa3@gmail.com>
2024-04-16 14:59:05 +0500

    change

--- backEnd/mainSectionWithPlaces.php
+++ backEnd/mainSectionWithPlaces.php
@@ -1,23 +1,45 @@
 <ul class="all-places-list">
-        <li class="searched-place-item">
-            <img class="items-image-places" src="static/images/imagesPlaces/cityMallMain.png" alt="">
+    <p class="count-finded-places">9 объектов найдено</p>    
+    <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/mallsPlaces/cityMallMain.png" alt="">
             <h4 class="name-place-section-all-places">Гостиный двор</h4>
             <p class="name-street-place">Уфа, Верхнеторговая площадь 1</p>
             <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
             <p class="score-count-text">4,6</p>
-        </li>
-        <li class="searched-place-item">
-            <img class="items-image-places" src="static/images/imagesPlaces/cityMallMain.png" alt="">
-            <h4 class="name-place-section-all-places">Отель Azimut</h4>
-            <p class="name-street-place">Уфа, Верхнеторговая площадь 1</p>
+        </a></li>
+        <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/hotelsPlaces/bashkiriaHotel.png" alt="">
+            <h4 class="name-place-section-all-places">Гостиница Башкирия</h4>
+            <p class="name-street-place">Уфа, Ленина 25/29</p>
             <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
-            <p class="score-count-text">4,6</p>
-        </li>
-            <li class="searched-place-item">
-            <img class="items-image-places" src="static/images/imagesPlaces/cityMallMain.png" alt="">
-            <h4 class="name-place-section-all-places">Гостиный двор</h4>
-            <p class="name-street-place">Уфа, Верхнеторговая площадь 1</p>
+            <p class="score-count-text">4,8</p>
+        </a></li>
+        <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/hotelsPlaces/nesterovplazaHotel.png" alt="">
+            <h4 class="name-place-section-all-places">Nesterov Plaza</h4>
+            <p class="name-street-place">Уфа, Верхнеторговая площадь 2</p>
             <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
-            <p class="score-count-text">4,6</p>
-        </li>
+            <p class="score-count-text">4,2</p>
+        </a></li>
+        <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/restaurantsPlaces/sherlockRestaurant.png" alt="">
+            <h4 class="name-place-section-all-places">Шерлок Холмс</h4>
+            <p class="name-street-place">Уфа, Чернышевского 75</p>
+            <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
+            <p class="score-count-text">4,5</p>
+        </a></li>
+        <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/parksPlaces/parkVictory.png" alt="">
+            <h4 class="name-place-section-all-places">Парк Победы</h4>
+            <p class="name-street-place">Уфа, Комарова 7</p>
+            <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
+            <p class="score-count-text">4,8</p>
+        </a></li>
+        <li class="searched-place-item"><a href="backEnd/hotelsCategory.php">
+        <img class="items-image-places" src="static/images/parksPlaces/parkVictory.png" alt="">
+            <h4 class="name-place-section-all-places">Котокафе "Котейная"</h4>
+            <p class="name-street-place">Уфа, 50-лет Октября 26</p>
+            <img class="heart-icon" src="static/images/icons/heartIcon.png" alt="">
+            <p class="score-count-text">5,0</p>
+        </a></li>
 </ul>
